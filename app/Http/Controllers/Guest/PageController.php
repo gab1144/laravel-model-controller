50b1154f39310302490dd2c4afb5611299a935ad
gab1144 <gabry.rinciari04@gmail.com>
2023-01-09 17:47:53 +0100

    [add] model, stampa titoli

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index(){
-        return view('movies');
+        $movies = Movie::all();
+        //dd($movies);
+        return view('movies', compact('movies'));
     }
 
     public function about(){
--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -4,6 +4,12 @@
     <main>
         <div class="container">
             <h1>Movies</h1>
+
+            <ul>
+                @foreach ($movies as $movie)
+                <li>{{$movie->title}}</li>
+                @endforeach
+            </ul>
         </div>
     </main>
 @endsection
