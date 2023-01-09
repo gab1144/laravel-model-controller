7daef40994f2f33d8c292a59ebbfc76826cf6c0a
gab1144 <gabry.rinciari04@gmail.com>
2023-01-09 17:24:04 +0100

    [add] controller

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('content')
+    <main>
+        <div class="container">
+            <h1>About</h1>
+        </div>
+    </main>
+@endsection
+
+@section('title')
+    About
+@endsection
---
+++ resources/views/movies.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('content')
+    <main>
+        <div class="container">
+            <h1>Movies</h1>
+        </div>
+    </main>
+@endsection
+
+@section('title')
+    Movies
+@endsection
