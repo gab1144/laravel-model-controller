e2dbe35c3e4d6b5d6ad706f69c699011c84988a9
gab1144 <gabry.rinciari04@gmail.com>
2023-01-09 17:57:06 +0100

    [add] stampa card

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -5,11 +5,21 @@
         <div class="container">
             <h1>Movies</h1>
 
-            <ul>
+            <div class="row">
+
                 @foreach ($movies as $movie)
-                <li>{{$movie->title}}</li>
+                    <div class="col-4 mb-3">
+                        <div class="card" >
+                            <div class="card-body">
+                              <h5 class="card-title text-black">{{$movie->title}}</h5>
+                              <h6 class="card-title text-black">{{$movie->vote}}/10</h6>
+                            </div>
+                          </div>
+                    </div>
+
                 @endforeach
-            </ul>
+
+            </div>
         </div>
     </main>
 @endsection
