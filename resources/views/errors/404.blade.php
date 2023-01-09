34bd617b66f9862af95fc7861cd2b186291a2699
gab1144 <gabry.rinciari04@gmail.com>
2023-01-09 18:21:05 +0100

    [add] bonus 2

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('content')
+    <main>
+        <div class="container">
+            <h1>Errore 404</h1>
+        </div>
+    </main>
+@endsection
+
+@section('title')
+    Error 404
+@endsection
